54f1a46cdb76f97f00be43d22c4f0cb72a284a37
Jatin Goyal <jg.silenttears.0786@gmail.com>
2022-06-06 22:19:11 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+dotenv.config({ path: './config.env' });
+require('./db/connect');
+// const User=require('./model/userSchema');
+app.use(express.json());
+app.use(require('./router/auth'));
+const PORT = process.env.PORT;
+
+//Middleware
+const middleware = (req, res, next) => {
+  console.log('My middleware');
+  next();
+};
+app.get('/', (req, res) => {
+  res.send('Hello from server');
+});
+
+app.get('/about', middleware, (req, res) => {
+  console.log('hello about');
+  res.send('Hello About from server');
+});
+
+app.get('/login', (req, res) => {
+  res.send('Hello Login from server');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
